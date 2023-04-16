7b2ede2c7af3deda679e9b27c0dec4933b5b8ae2
Luis Ezequiel Vélez <92954990+ezvel@users.noreply.github.com>
2023-04-16 03:20:54 -0300

    Agrega estructura html fundamental
    Se necesita comprobar si las áreas o los links generales del programa funciona.

---
+++ inscripcion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+		<h1>CAMPEONATO DE CHINCHON</h1>
+		<nav>
+			<ul>
+				<li><a href="./inscripcion.php">INSCRIPCION</a></li>
+				<li><a href="./participantes.php">PARTICIPANTES</a></li>
+				<li><a href="./partidas.php">PARTIDAS</a></li>
+				<li><a href="./posiciones.php">POSICIONES</a></li>
+				<li><a href="./seccion_administradores.php">SECCION ADMINISTRACION</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h2>Inscripcion</h2>
+	</main>
+</body>
+</html>
